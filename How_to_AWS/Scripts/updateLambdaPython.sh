57a5ae023a65da57ac2a5bfd291f7d476239b133
Ulysse-C <65217842+Ulysse-C@users.noreply.github.com>
2020-05-25 10:01:08 +0200

    Update and rename updateLambda.sh to updateLambdaPython.sh

--- How_to_AWS/Scripts/updateLambdaPython.sh
+++ How_to_AWS/Scripts/updateLambdaPython.sh
@@ -1,9 +1,9 @@
-#$1 : path to your function, $2 name of the function (on aws), $3 extension of the function
+#$1 : path to your function, $2 name of the function (on aws)
 cd $1
 cd package
 zip -r9 ${OLDPWD}/function.zip .
 cd $OLDPWD
-zip -g function.zip *.$3
+zip -g function.zip *.py
 aws lambda update-function-code --function-name $2 --zip-file fileb://./function.zip
 rm function.zip
 cd $OLDPWD
