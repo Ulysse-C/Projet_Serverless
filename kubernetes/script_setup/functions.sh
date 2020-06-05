c5f6a75a39a68ba273d54589e3e7f1926c312a26
Ulysse-C <65217842+Ulysse-C@users.noreply.github.com>
2020-06-05 14:44:24 +0200

    Update functions.sh

--- kubernetes/script_setup/functions.sh
+++ kubernetes/script_setup/functions.sh
@@ -1,5 +1,6 @@
 kubeless function deploy webtokafka --runtime nodejs12 --from-file WebtoKafka.js --handler WebtoKafka.tokafka -d package.json
 kubeless function deploy kafkatoes --runtime python3.7 --from-file KafkatoES.py --handler KafkatoES.handler -d requirements.txt
+kubeless topic create rte-france
 sleep 3s
-kubeless trigger kafka create toes --function-selector created-by=kubeless,function=kafkatoes --trigger-topic ret-france
+kubeless trigger kafka create toes --function-selector created-by=kubeless,function=kafkatoes --trigger-topic rte-france
 kubeless trigger cronjob create tokafka --function webtokafka --schedule "*/30 * * * *"
